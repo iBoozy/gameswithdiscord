84f0df529d190040fbfad01ca9a772ab5c215f63
helmetheroes <camzengamer@gmail.com>
2019-07-18 21:30:45 -0400

    3

--- sharetest/data/sharing.js
+++
@@ -1,7 +0,0 @@
-self.port.on("ShareOn", function () {
-  document.getElementById('share').innerHTML = 'sharing...';
-});
-
-self.port.on("ShareOff", function () {
-  document.getElementById('share').innerHTML = 'share';
-});
